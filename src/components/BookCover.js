1ebc4bb0f0e9e0f1ea4538997890a3d8a911b18a
Josiah Ufono <josiahufono@gmail.com>
2021-03-22 06:40:52 +0100

    Update BookCover.js

--- src/components/BookCover.js
+++ src/components/BookCover.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
-import Bookshelf from './Bookshelf';
+import BookShelf from './BookShelf';
 
 class BookCover extends Component {
 
@@ -16,9 +16,9 @@ class BookCover extends Component {
     return (
       <div className='list-books-content'>
         <div>
-          <Bookshelf books={this.state.books} shelfType='currentlyReading' shelfName='Currently Reading' onBookshelfChange={this.props.onBookshelfChange}/>
-					<Bookshelf books={this.state.books} shelfType='wantToRead' shelfName='Want to Read' onBookshelfChange={this.props.onBookshelfChange}/>
-					<Bookshelf books={this.state.books} shelfType='read' shelfName='Read' onBookshelfChange={this.props.onBookshelfChange}/>
+          <BookShelf books={this.state.books} shelfType='currentlyReading' shelfName='Currently Reading' onBookshelfChange={this.props.onBookshelfChange}/>
+					<BookShelf books={this.state.books} shelfType='wantToRead' shelfName='Want to Read' onBookshelfChange={this.props.onBookshelfChange}/>
+					<BookShelf books={this.state.books} shelfType='read' shelfName='Read' onBookshelfChange={this.props.onBookshelfChange}/>
         </div>
       </div>
     );
